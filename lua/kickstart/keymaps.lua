c2d30293c34a440597f7400956a0133d272cfec7
Fran Svarc <svarcf@gmail.com>
2024-09-17 02:21:32 +0200

    added persistance

--- lua/kickstart/keymaps.lua
+++ lua/kickstart/keymaps.lua
@@ -58,6 +58,26 @@ vim.keymap.set('n', '<leader>tir', ':TSToolsRemoveUnusedImports<CR>', { desc = '
 vim.keymap.set('n', '<leader>tu', ':TSToolsRemoveUnused<CR>', { desc = '[T]ypescript [U]nused' })
 vim.keymap.set('n', '<leader>tf', ':TSToolsFixAll<CR>', { desc = '[T]ypescript [F]ix' })
 
+-- load the session for the current directory
+vim.keymap.set('n', '<leader>qs', function()
+  require('persistence').load()
+end)
+
+-- select a session to load
+vim.keymap.set('n', '<leader>qS', function()
+  require('persistence').select()
+end)
+
+-- load the last session
+vim.keymap.set('n', '<leader>ql', function()
+  require('persistence').load { last = true }
+end)
+
+-- stop Persistence => session won't be saved on exit
+vim.keymap.set('n', '<leader>qd', function()
+  require('persistence').stop()
+end)
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- lua/kickstart/lazy-plugins.lua
+++ lua/kickstart/lazy-plugins.lua
@@ -775,6 +775,13 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'folke/persistence.nvim',
+    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
+    opts = {
+      -- add any custom options here
+    },
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
