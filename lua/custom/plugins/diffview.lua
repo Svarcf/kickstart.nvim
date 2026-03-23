05864000b62763873ffa53c23adca6c1501cff31
Svarcf <svarcf@gmail.com>
2026-03-23 14:34:10 +0100

    adjusted the way panes are split in diff

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -2,19 +2,40 @@ return {
   'sindrets/diffview.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- optional but nice
+    'nvim-tree/nvim-web-devicons',
   },
   keys = {
     { '<leader>gv', '<cmd>DiffviewOpen<cr>', desc = 'Git Diff (repo)' },
-    { '<leader>gV', '<cmd>DiffviewFileHistory %<cr>', desc = 'File History (current file)' },
+    {
+      '<leader>gV',
+      '<cmd>DiffviewFileHistory % --panel-layout=diff2_vertical --file-panel-position=bottom<cr>',
+      desc = 'File History (current file) bottom panel',
+    },
     { '<leader>gq', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
   },
   config = function()
     require('diffview').setup {
       enhanced_diff_hl = true,
       use_icons = true,
-      file_panel = { position = 'left', width = 35 },
-      file_history_panel = { position = 'right', width = 40 },
+      view = {
+        -- ensures diff2_vertical is available for two side-by-side diffs
+        default = {
+          layout = 'diff2_vertical',
+        },
+        merge_tool = {
+          layout = 'diff2_vertical',
+          disable_diagnostics = true,
+        },
+      },
+      file_panel = {
+        position = 'bottom', -- place file panel (history) below the diffs
+        listing_style = 'tree',
+        win_config = { height = 12 },
+      },
+      file_history_panel = {
+        position = 'bottom',
+        win_config = { height = 12 },
+      },
       signs = {
         fold_closed = '▸',
         fold_open = '▾',
